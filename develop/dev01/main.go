5a7974e11e604e8414ae391ab8d841503c0ee8e0
valery <ebalaika1994@gmail.com>
2022-06-24 15:51:17 +0300

    dev 01

---
+++ develop/dev01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+)
+
+func main() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s", err)
+	}
+	fmt.Println(time)
+}
